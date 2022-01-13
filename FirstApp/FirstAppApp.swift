27dd4e2c8f4c749de0ae2e9555cd59bf41d68a08
iTao <itao_1314@icloud.com>
2022-01-13 14:34:34 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 陶涛 on 2022/1/13.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
