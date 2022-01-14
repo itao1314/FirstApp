c17123770f9723f29dbd0cf1eda6d2a91b073e77
iTao <itao_1314@icloud.com>
2022-01-14 15:56:46 +0800

    初始化项目

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by 陶涛 on 2022/1/14.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
--- FirstApp/FirstAppApp.swift
+++ FirstApp/FirstAppApp.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+// @main标记了FirstAppApp作为程序的入口
+// 就像很多的语言的main函数一样，在SwiftUI中App协议做了这件事
 @main
+
+// App要求计算属性body返回一个Scene,Scene是一个加载根视图的容器。
 struct FirstAppApp: App {
     var body: some Scene {
+        // 对于iOS App来说，默认设置WindowGroup包含一个ContentView作为根视图
         WindowGroup {
             ContentView()
         }
